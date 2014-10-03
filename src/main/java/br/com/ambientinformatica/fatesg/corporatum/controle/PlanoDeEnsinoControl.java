fa749f800a632ad2f30e1c8cfedcaf28f2a973d9
rodrigo <rodrigo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-10-03 00:14:35 +0000

    Adicionando a tela de Plano de ensino e suas classes(controle e persistencia).

---
+++ src/main/java/br/com/ambientinformatica/fatesg/corporatum/controle/PlanoDeEnsinoControl.java
@@ -0,0 +1,62 @@
+package br.com.ambientinformatica.fatesg.corporatum.controle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.event.ActionEvent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
+import br.com.ambientinformatica.fatesg.api.PlanoDeEnsino;
+import br.com.ambientinformatica.fatesg.corporatum.persistencia.PlanoDeEnsinoDao;
+
+@Controller("PlanoDeEnsinoControl")
+@Scope("conversation")
+public class PlanoDeEnsinoControl {
+
+	private PlanoDeEnsino planoDeEnsino  = new PlanoDeEnsino();
+	@Autowired
+	private PlanoDeEnsinoDao planoDeEnsinoDao;
+	
+	private List<PlanoDeEnsino> planosDeEnsino = new ArrayList<PlanoDeEnsino>();
+	
+
+   @PostConstruct
+   public void init(){
+      listar(null);
+   }
+   
+	public void confirmar(ActionEvent evt){
+		try {
+			planoDeEnsinoDao.alterar(planoDeEnsino);
+         listar(evt);
+         planoDeEnsino = new PlanoDeEnsino();
+		} catch (Exception e) {
+		   UtilFaces.addMensagemFaces(e);
+		}
+	}
+
+	public void listar(ActionEvent evt){
+		try {
+			planosDeEnsino = planoDeEnsinoDao.listar();
+		} catch (Exception e) {
+		   UtilFaces.addMensagemFaces(e);
+		}
+	}
+	
+	public PlanoDeEnsino getPlanoDeEnsino1() {
+		return planoDeEnsino;
+	}
+
+	public void setPlanoDeEnsino(PlanoDeEnsino planoDeEnsino) {
+		this.planoDeEnsino = planoDeEnsino;
+	}
+	
+	public List<PlanoDeEnsino> getPlanosDeEnsino() {
+		return planosDeEnsino;
+	}	
+}
