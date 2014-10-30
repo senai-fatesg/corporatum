e642042e4d782e307e0cef78a7dbbe44db5b01e1
glaicon <glaicon@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-10-30 21:41:44 +0000

    metodo para verificar os campos vazios

--- src/main/java/br/com/ambientinformatica/fatesg/corporatum/persistencia/AlunoDao.java
+++ src/main/java/br/com/ambientinformatica/fatesg/corporatum/persistencia/AlunoDao.java
@@ -4,5 +4,7 @@ import br.com.ambientinformatica.jpa.persistencia.Persistencia;
 import br.com.ambientinformatica.fatesg.api.Aluno;
 
 public interface AlunoDao extends Persistencia<Aluno>{
+	
+	public void verificarCampos(Aluno aluno);
 
 }
