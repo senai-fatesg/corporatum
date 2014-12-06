2e48e1c97b9ded2bf2031db33c5936606f452e6d
rodrigo <rodrigo@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2014-12-06 12:04:52 +0000

    Refatorada a tela disciplina.jspx e criada a tela disciplinalista.jspx

---
+++ src/main/java/br/com/ambientinformatica/fatesg/corporatum/util/DisciplinaConverter.java
@@ -0,0 +1,55 @@
+package br.com.ambientinformatica.fatesg.corporatum.util;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
+import br.com.ambientinformatica.fatesg.api.dao.DisciplinaDao;
+import br.com.ambientinformatica.fatesg.api.entidade.Disciplina;
+import br.com.ambientinformatica.jpa.exception.PersistenciaException;
+import br.com.ambientinformatica.jpa.util.FabricaAbstrata;
+
+@FacesConverter("disciplinaConverter")
+public class DisciplinaConverter implements Converter {
+	
+	
+	   private DisciplinaDao disciplinaDao = (DisciplinaDao)FabricaAbstrata.criarObjeto("DiciplinaDao");
+	   
+	   @Override
+	   public String getAsString(FacesContext facesContext, UIComponent component, Object value) {  
+	       if (value == null || value.equals("")) {  
+	           return "";  
+	       } else {  
+	           return String.valueOf(((Disciplina)value).getId());  
+	       }  
+	   }
+
+
+	   @Override
+	   public Object getAsObject(FacesContext context, UIComponent component, String value) {
+	      if (value != null && !value.trim().equals("")) {  
+			Disciplina disciplina = new Disciplina();
+	         try {  
+	         	long id = Long.parseLong(value);  
+
+	            try {
+	            	disciplina = disciplinaDao.consultar(id);
+	            } catch (PersistenciaException e) {
+	               e.printStackTrace();
+	            }
+	         } catch(NumberFormatException exception) {  
+	            UtilFaces.addMensagemFaces("Avaliação escolhida não é válida. Erro no Converter");
+	            //return null;
+	         }  
+	         return disciplina;  
+	      }else{
+	         return null;
+	      }
+	   }
+	}  
+
+
+
+
